34fe978b804d0dd9e66884301955f57912987a1d
ricahcyuzuzo <ricahcyuzuzo@gmail.com>
2022-04-13 18:03:29 +0200

    Design of Remaining Auth Screens

--- src/pages/confirm.jsx
+++ src/pages/confirm.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-const Confirm = () => {
+const Confirm = ({match}) => {
+  useEffect(() => {
+    const {id} = match.params;
+    console.log('Props ID ', id)
+  }, [match.params]);
+
   return (
     <div>
       <h1>Confirm Email</h1>
